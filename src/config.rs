1fd968bc87ab2e69abcf1fc931b87e63d808b61e
CRBl_ <crbl.me@gmail.com>
2021-09-17 00:48:33 +0200

    Added errors for non-valid argument values

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use crate::ColorHSV;
 use clap::App;
+use colored::{Color::Red, Colorize};
 
 pub struct Config {
     pub file: String,
@@ -17,27 +18,64 @@ pub fn get_args() -> Config {
 
     let file = matches.value_of("FILE").unwrap_or("").to_string();
 
+    if !file.is_empty() && std::fs::metadata(&file).is_err() {
+        eprintln!(
+            "[{}]: Given file ({}) doesn't exist or you do not have the permissions to access this file",
+            "ERROR".color(Red),
+            file
+        );
+        std::process::exit(1);
+    }
+
     let text = matches.value_of("text").unwrap_or("").to_string();
     let hue = matches
         .value_of("hue")
         .unwrap_or("0")
         .parse::<f32>()
-        .unwrap();
+        .unwrap_or_else(|_| {
+            eprintln!(
+                "[{}]: hue must be a float, got \"{}\"",
+                "ERROR".color(Red),
+                matches.value_of("hue").unwrap()
+            );
+            std::process::exit(1);
+        });
     let value = matches
         .value_of("value")
         .unwrap_or("1")
         .parse::<f32>()
-        .unwrap();
+        .unwrap_or_else(|_| {
+            eprintln!(
+                "[{}]: value must be a float, got \"{}\"",
+                "ERROR".color(Red),
+                matches.value_of("value").unwrap()
+            );
+            std::process::exit(1);
+        });
     let saturation = matches
         .value_of("saturation")
         .unwrap_or("1")
         .parse::<f32>()
-        .unwrap();
+        .unwrap_or_else(|_| {
+            eprintln!(
+                "[{}]: saturation must be a float, got \"{}\"",
+                "ERROR".color(Red),
+                matches.value_of("saturation").unwrap()
+            );
+            std::process::exit(1);
+        });
     let radius = matches
         .value_of("radius")
         .unwrap_or("1")
         .parse::<f32>()
-        .unwrap();
+        .unwrap_or_else(|_| {
+            eprintln!(
+                "[{}]: radius must be a float, got \"{}\"",
+                "ERROR".color(Red),
+                matches.value_of("radius").unwrap()
+            );
+            std::process::exit(1);
+        });
 
     let background = matches!(matches.occurrences_of("bg"), 1);
     let rainbow = matches!(matches.occurrences_of("rainbow"), 1);
